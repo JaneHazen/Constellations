87b803d9a3df9995d489690ed10e7850c8413787
v-janwil <iamjane@gmail.com>
2018-06-25 21:44:24 -0700

    add styles

--- src/App.js
+++ src/App.js
@@ -3,52 +3,163 @@ import './App.css';
 import Sky from './Sky';
 
 class App extends Component {
-    constructor(){
-        super();
-        this.state = {
-            locationOfStars: [],
-            mapOfEdges: [],
-        };
+	constructor() {
+		super();
 
-        this.clickedVirgo = this.clickedVirgo.bind(this);
-    }
+		this.state = {
+			locationOfStars: [],
+			mapOfEdges: [],
+			skyHeight: undefined,
+			skyWidth: undefined
+		};
 
-    clickedVirgo(){
-        this.setState({
-            locationOfStars:[
-                {mouseX:138, mouseY:474},
-                {mouseX:149, mouseY:435},
-                {mouseX:170, mouseY: 487},
-                {mouseX:225, mouseY: 530},
-                {mouseX:189, mouseY:451},
-                {mouseX:224, mouseY:477},
-                {mouseX:279, mouseY:477},
-                {mouseX:306, mouseY: 497},
-                {mouseX:335, mouseY:492},
-                {mouseX:273, mouseY: 473},
-                {mouseX:272, mouseY:438}
-            ],
-            mapOfEdges: [
-                {0: ["2", "1"]},
-                {2: ["0", "4"]},
-                {3: ["3", "0"]},
-                {4: ["6", "2"]},
-                {5: ["1", "5"]},
-                {6: ["4"]},
-                {7: ["7", "9", "3"]},
-                {8: ["8", "6"]},
-                {9: ["7"]},
-                {10: ["6", "10"]},
-                {11: ["9"]}
-            ]
-        })
-    }
+		this.clickedHercules = this.clickedHercules.bind(this);
+		this.clickedGemini = this.clickedGemini.bind(this);
+		this.clickedClear = this.clickedClear.bind(this);
+		this.fitStageIntoParentContainer = this.fitStageIntoParentContainer.bind(this);
+	}
 
-    render() {
-        return (
-                    <Sky className='skyBox' locationOfStars={this.state.locationOfStars} mapOfEdges={this.state.mapOfEdges} clickedVirgo={this.clickedVirgo}/>
-        );
-    }
+	clickedClear(){
+		this.setState({
+			locationOfStars:[],
+			mapOfEdges:[]
+		});
+	}
+
+	clickedGemini(){
+		this.setState({
+			locationOfStars: [
+				{mouseX: 538, mouseY: 362},
+				{mouseX: 539, mouseY: 344},
+				{mouseX: 562, mouseY: 341},
+				{mouseX: 594, mouseY: 345},
+				{mouseX: 616, mouseY: 341},
+				{mouseX: 623, mouseY: 334},
+				{mouseX: 629, mouseY: 324},
+				{mouseX: 625, mouseY: 389},
+				{mouseX: 621, mouseY: 372},
+				{mouseX: 587, mouseY: 372},
+				{mouseX: 566, mouseY: 379}
+			],
+			mapOfEdges: [
+				[],
+				["1"],
+				["1","2"],
+				["2","3"],
+				["3","4"],
+				["4","5"],
+				["5","6"],
+				["8"],
+				["9"],
+				["10"],
+				["0"]
+			]
+		})
+	}
+
+	clickedHercules() {
+		this.setState({
+			locationOfStars: [
+				{mouseX: 102, mouseY: 142},
+				{mouseX: 108, mouseY: 156},
+				{mouseX: 103, mouseY: 171},
+				{mouseX: 99, mouseY: 194},
+				{mouseX: 102, mouseY: 218},
+				{mouseX: 139, mouseY: 218},
+				{mouseX: 151, mouseY: 237},
+				{mouseX: 186, mouseY: 219},
+				{mouseX: 161, mouseY: 194},
+				{mouseX: 154, mouseY: 150},
+				{mouseX: 203, mouseY: 158},
+				{mouseX: 223, mouseY: 221},
+				{mouseX: 219, mouseY: 247},
+				{mouseX: 158, mouseY: 256},
+				{mouseX: 45, mouseY: 247},
+				{mouseX: 112, mouseY: 278}
+			],
+			mapOfEdges: [
+				[],
+				["0"],
+				["1"],
+				["2"],
+				["3"],
+				["4"],
+				["5"],
+				["6"],
+				["7", "5"],
+				["8"],
+				["9"],
+				["7"],
+				["11"],
+				["6"],
+				["15","4"],
+				["6"]
+			]
+		})
+	}
+
+	componentDidMount(){
+		this.fitStageIntoParentContainer();
+		window.addEventListener("resize", this.fitStageIntoParentContainer.bind(this));
+	}
+
+	componentWillUnmount(){
+		window.removeEventListener('resize', this.fitStageIntoParentContainer.bind(this));
+	}
+
+	fitStageIntoParentContainer() {
+		let parent = document.getElementById("leftPanelContainer");
+		let padding = 50;
+
+		let height = parent.clientHeight - padding;
+		let width = parent.clientWidth - padding;
+
+
+		setTimeout(function() { this.setState({skyHeight: height, skyWidth: width}); }.bind(this), 500);
+	}
+
+	getSkyBox(){
+		return(
+			<Sky className='skyBox' stageHeight={this.state.skyHeight}
+				 stageWidth={this.state.skyWidth}
+				 locationOfStars={this.state.locationOfStars}
+				 mapOfEdges={this.state.mapOfEdges}
+				 />
+		)
+	}
+
+	render() {
+		return (
+			<div className="app">
+				<div className="appContainer">
+					<div className="radioContainer">
+						<div className="topContainer">
+							<div id="leftPanelContainer" className="leftPanelContainer">
+								<div id="skyContainer">
+									{this.state.skyHeight ? this.getSkyBox() : ""}
+								</div>
+							</div>
+							<div className="rightPanelContainer">
+								<div className="buttonContainer">
+									<button onClick={this.clickedHercules}>Hercules</button>
+									<button onClick={this.clickedGemini}>Gemini</button>
+									<button onClick={this.clickedClear}>Clear</button>
+								</div>
+
+							</div>
+						</div>
+						<div className="footer">
+							<div className="titlebox">
+								<div className="title">
+									<h1>Constellation Maker 3005</h1>
+								</div>
+							</div>
+						</div>
+					</div>
+				</div>
+			</div>
+		);
+	}
 }
 
 export default App;
--- src/Sky.js
+++ src/Sky.js
@@ -1,281 +1,236 @@
 import React from 'react';
-import {Layer, Line, Rect, Stage, Star, Text, Label, Tag} from 'react-konva';
+import {Layer, Line, Stage, Star, Image} from 'react-konva';
 
 
 class Sky extends React.Component {
-    constructor(props){
-        super(props);
-        const image = new window.Image();
-        image.onload = () => {
-            const dim = this.getViewPortDimension();
-            this.setState({
-                fillPatternImage: image,
-                viewportWidth: dim.w,
-                viewportHeight: dim.h,
-            });
-        };
-        image.src = 'https://i.imgur.com/ewWyxVH.png';
-        this.state = {
-            locationOfStars: this.props.locationOfStars,
-            firstEdge: undefined,
-            mapOfEdges: this.props.mapOfEdges,
-            originalX: undefined,
-            originalY: undefined,
-            fillPatternImage: null,
-            viewportWidth: 400,
-            viewportHeight: 400,
-        };
+	constructor(props) {
+		super(props);
+		this.state = {
+			locationOfStars: this.props.locationOfStars ? this.props.locationOfStars : [],
+			firstEdge: undefined,
+			mapOfEdges: this.props.mapOfEdges ? this.props.mapOfEdges : [],
+			originalX: undefined,
+			originalY: undefined,
+			fillPatternImage: null,
+			stageHeight: this.props.stageHeight,
+			stageWidth: this.props.stageWidth,
+		};
+	}
 
-        window.onresize = (event) => {
-            const dim = this.getViewPortDimension();
+	componentWillReceiveProps(nextProps) {
+		this.setState({
+			stageHeight: nextProps.stageHeight,
+			stageWidth: nextProps.stageWidth,
+			scale: nextProps.scale,
+			locationOfStars: nextProps.locationOfStars,
+			mapOfEdges: nextProps.mapOfEdges
+		})
+	}
 
-            this.setState({
-                viewportWidth: dim.w,
-                viewportHeight: dim.h,
-            });
-        };
-    }
-
-    getViewPortDimension() {
-        const w = Math.max(document.documentElement.clientWidth, window.innerWidth || 0);
-        const h = Math.max(document.documentElement.clientHeight, window.innerHeight || 0);
-        return {w, h};
-    }
-
-    componentWillReceiveProps(nextProps){
-        this.setState({
-            locationOfStars: nextProps.locationOfStars,
-            mapOfEdges: nextProps.mapOfEdges
-        })
-    }
+	componentWillMount(){
+		const image = new window.Image();
+		image.onload = () => {
+			this.setState({
+				fillPatternImage: image,
+			});
+		};
+		image.height = this.state.height;
+		image.width = this.state.width;
+		image.src = 'https://i.imgur.com/ewWyxVH.png';
+	}
 
 
-    componentDidMount(){
-        this.update();
-    }
+	componentDidMount() {
+		this.update();
+	}
 
-    clickLine = (e) => {
-        this.update(e);
+	clickLine = (e) => {
+		this.update(e);
 
-    };
+	};
 
-    drawEdges(){
-        if(!this.state.mapOfEdges){
-            return;
-        }
+	drawEdges() {
+		if (!this.state.mapOfEdges) {
+			return;
+		}
+		if(!this.state.locationOfStars){
+			return;
+		}
 
-        let linesToRender = [];
-        let map = this.state.mapOfEdges;
-        let locations = this.state.locationOfStars;
+		let linesToRender = [];
+		let map = this.state.mapOfEdges;
+		let locations = this.state.locationOfStars;
 
-        let firstPoints = Object.keys(map);
-        for(let i = 0; i < map.length; i++){
-            let firstPoint = firstPoints[i];
-            if(map[firstPoint]) {
-                for (let j = 0; j < map[firstPoint].length; j++) {
-                    let secondPoint = map[firstPoint][j];
-                    linesToRender.push(<Line index={j.toString()}
-                                             stroke='#CACF85'
-                                             onClick = {(event) => this.clickLine(event)}
-                                             strokeWidth={5}
-                                             firstStar= {firstPoint}
-                                             secondStar ={secondPoint}
-                                             points={[locations[firstPoint].mouseX, locations[firstPoint].mouseY,
-                                             locations[secondPoint].mouseX, locations[secondPoint].mouseY]}/>)
+		let firstPoints = Object.keys(map);
+		for (let i = 0; i < map.length; i++) {
+			let firstPoint = firstPoints[i];
+			if (locations[firstPoint]) {
+				for (let j = 0; j < map[firstPoint].length; j++) {
+					let secondPoint = map[firstPoint][j];
+					linesToRender.push(<Line index={j.toString()}
+											 stroke='#CACF85'
+											 onClick={(event) => this.clickLine(event)}
+											 strokeWidth={5}
+											 firstStar={firstPoint}
+											 secondStar={secondPoint}
+											 points={[locations[firstPoint].mouseX, locations[firstPoint].mouseY,
+											 locations[secondPoint].mouseX, locations[secondPoint].mouseY]}/>)
+				}
+			}
+		}
+		return linesToRender;
+	}
 
-                }
-            }
-        }
-        return linesToRender;
-    }
+	getOriginalCoordinates(event) {
+		this.setState({
+				originalX: event.evt.offsetX,
+				originalY: event.evt.offsetY
+			}
+		)
+	}
 
-    getOriginalCoordinates(event) {
-        this.setState({
-                originalX: event.evt.clientX,
-                originalY: event.evt.clientY
-            }
-        )
-    }
+	update = (event) => {
+		const layer = this.refs.edgesLayer;
+		if (!event) {
+			return;
+		}
 
-    update = (event) => {
-        const layer = this.refs.edgesLayer;
-        if (!event) {
-            return;
-        }
+		if (event.target.attrs.className === "sky") {
+			this.setState({
+				locationOfStars: [...this.state.locationOfStars, {
+					mouseX: event.evt.offsetX,
+					mouseY: event.evt.offsetY
+				}],
+			});
+		}
 
-        if(event.target.attrs.className ==="sky"){
-            this.setState({
-                locationOfStars: [...this.state.locationOfStars, {mouseX: event.evt.clientX, mouseY: event.evt.clientY}],
-            });
-        }
+		if (event.target.className === "Line") {
+			console.log(event.target.id, "event.target.id onClickedLine");
+			console.log(this.state.mapOfEdges, "map of edges");
+			layer.draw();
+			return;
+		}
 
-        if(event.target.className === "Line"){
-            console.log(event.target.id, "event.target.id onClickedLine");
-            console.log(this.state.mapOfEdges, "map of edges")
-            layer.draw();
-            return;
-        }
+		if (event.target.className === "Star") {
+			this.handleClickStar(event);
+		}
 
-        if(event.target.className === "Star"){
-            this.handleClickStar(event);
-        }
+		// let starID = event.target.attrs.id;
+		// let fixedStarCoords = this.state.locationOfStars[starID];
+		// let currentMouseX = event.evt.clientX;
+		// let currentMouseY = event.evt.clientY;
+		// let newXPoint = fixedStarCoords.mouseX + currentMouseX - this.state.originalX;
+		// let newYPoint = fixedStarCoords.mouseY + currentMouseY - this.state.originalY;
+		// let map = this.state.mapOfEdges;
+		// let starLines = layer.children;
+		//
+		// if (starLines.length > 0) {
+		//     for (let i = 0; i < starLines.length; i++) {
+		//         let edge = starLines[i];
+		//         if (map[starID]) {
+		//             for (let j = 0; j < map[starID].length; j++) {
+		//                 let movingStarCoords = this.state.locationOfStars[map[starID][j]];
+		//                 if (edge.attrs.firstStar === starID) {
+		//                     starLines[j].attrs.points = [newXPoint, newYPoint, movingStarCoords.mouseX, movingStarCoords.mouseY];
+		//                     layer.add(starLines[j]);
+		//                 } else if (edge.attrs.secondStar === starID) {
+		//                     starLines[j].attrs.points = [movingStarCoords.mouseX, movingStarCoords.mouseY, newXPoint, newYPoint];
+		//                     layer.add(starLines[j]);
+		//                 }
+		//             }
+		//         }
+		//
+		//     }
+		// }
+		// layer.draw();
+	};
 
+	handleClickStar(event) {
+		if (event.type === "dragmove") {
+			this.setStarLocationsToMousePosition(event);
+		} else {
+			if (!!this.state.firstEdge) {
+				let vertex1 = this.state.firstEdge;
+				let vertex2 = event.target.attrs.id;
+				let mapOfEdges = this.state.mapOfEdges;
 
-        // let starID = event.target.attrs.id;
-        // let fixedStarCoords = this.state.locationOfStars[starID];
-        // let currentMouseX = event.evt.clientX;
-        // let currentMouseY = event.evt.clientY;
-        // let newXPoint = fixedStarCoords.mouseX + currentMouseX - this.state.originalX;
-        // let newYPoint = fixedStarCoords.mouseY + currentMouseY - this.state.originalY;
-        // let map = this.state.mapOfEdges;
-        // let starLines = layer.children;
-        //
-        // if (starLines.length > 0) {
-        //     for (let i = 0; i < starLines.length; i++) {
-        //         let edge = starLines[i];
-        //         if (map[starID]) {
-        //             for (let j = 0; j < map[starID].length; j++) {
-        //                 let movingStarCoords = this.state.locationOfStars[map[starID][j]];
-        //                 if (edge.attrs.firstStar === starID) {
-        //                     starLines[j].attrs.points = [newXPoint, newYPoint, movingStarCoords.mouseX, movingStarCoords.mouseY];
-        //                     layer.add(starLines[j]);
-        //                 } else if (edge.attrs.secondStar === starID) {
-        //                     starLines[j].attrs.points = [movingStarCoords.mouseX, movingStarCoords.mouseY, newXPoint, newYPoint];
-        //                     layer.add(starLines[j]);
-        //                 }
-        //             }
-        //         }
-        //
-        //     }
-        // }
-        // layer.draw();
-    }
+				mapOfEdges[vertex1] ? mapOfEdges[vertex1].push(vertex2) : mapOfEdges[vertex1] = [vertex2];
 
-    handleClickStar(event) {
-        if (event.type === "dragmove") {
-            this.setStarLocationsToMousePosition(event);
-        } else {
-            if (!!this.state.firstEdge) {
-                let vertex1 = this.state.firstEdge;
-                let vertex2 = event.target.attrs.id;
-                let mapOfEdges = this.state.mapOfEdges;
+				this.setState({
+					firstEdge: undefined,
+					mapOfEdges: mapOfEdges
+				})
+			} else {
+				let id = event.target.attrs.id;
+				this.setState({
+					firstEdge: id,
+				});
+			}
+		}
+	}
 
-                mapOfEdges[vertex1] ? mapOfEdges[vertex1].push(vertex2) : mapOfEdges[vertex1] = [vertex2];
+	setStarLocationsToMousePosition(event) {
+		let locationOfStars = this.state.locationOfStars;
+		locationOfStars[event.target.attrs.id] = {mouseX: event.evt.offsetX, mouseY: event.evt.offsetY};
+		this.setState({
+			locationOfStars: locationOfStars,
+			firstEdge: undefined,
+		});
+	}
 
-                this.setState({
-                    firstEdge: undefined,
-                    mapOfEdges: mapOfEdges
-                })
-            } else {
-                console.log("set first edge")
-                let id = event.target.attrs.id;
-                this.setState({
-                    firstEdge: id,
-                });
-            }
-        }
-    }
+	drawStars() {
+		let starLocations = this.state.locationOfStars;
+		if (!starLocations) {
+			return;
+		}
+		let stars = [];
+		for (let i = 0; i < starLocations.length; i++) {
+			stars.push(<Star
+				className="star"
+				onMouseEnter={() => {
+					document.body.style.cursor = "pointer";
+				}}
+				onMouseLeave={() => {
+					document.body.style.cursor = "default";
+				}}
+				id={i.toString()}
+				x={starLocations[i].mouseX}
+				y={starLocations[i].mouseY}
+				numPoints={6}
+				innerRadius={6}
+				outerRadius={10}
+				onDragstart={(event) => this.getOriginalCoordinates(event)}
+				onDragMove={(event) => this.update(event)}
+				fill="#1D4BB7"
+				stroke='white'
+				draggable={true}
+				onClick={(event) => this.update(event)}
+			/>);
+		}
+		return stars;
+	}
 
-    setStarLocationsToMousePosition(event) {
-        let locationOfStars = this.state.locationOfStars;
-        locationOfStars[event.target.attrs.id] = {mouseX: event.evt.clientX, mouseY: event.evt.clientY};
-        this.setState({
-            locationOfStars: locationOfStars,
-            firstEdge: undefined,
-        });
-    }
-
-    drawStars(){
-        let starLocations = this.state.locationOfStars;
-        if (starLocations.length === 0) {
-            return;
-        }
-        let stars =[];
-        let starColor = '#C1666B';
-        for (let i = 0; i < starLocations.length; i++) {
-            stars.push(<Star
-                className="star"
-                onMouseEnter={() => {
-                    document.body.style.cursor = "pointer";
-                }}
-                onMouseLeave={() => {
-                    document.body.style.cursor = "default";
-                }}
-                id={i.toString()}
-                x={starLocations[i].mouseX}
-                y={starLocations[i].mouseY}
-                numPoints={6}
-                innerRadius={6}
-                outerRadius={10}
-                onDragstart = { (event) => this.getOriginalCoordinates(event) }
-                onDragMove = { (event) => this.update(event)}
-                fill={starColor}
-                stroke='white'
-                draggable={true}
-                onClick = { (event) => this.update(event)}
-            />);
-        }
-        return stars;
-    }
-
-    getView(){
-        let skyHeight = 677;
-        let skyWidth = 800;
-        let viewHeight = this.state.viewportHeight;
-        let viewWidth = this.state.viewportWidth;
-        console.log("MAP", this.state.mapOfEdges);
-        console.log("Stars", this.state.locationOfStars);
-        if(viewHeight <= skyHeight || viewWidth <= skyWidth){
-            return(
-                <Stage width={viewWidth} height={viewHeight}>
-                    <Layer>
-                        <Rect x={0} y={0} width={viewWidth} height={viewHeight} className="sky"
-                              fillPatternImage={this.state.fillPatternImage}
-                              onClick={(event) => this.update(event)}/>
-                    </Layer>
-                    <Layer ref={"edgesLayer"}>
-                        {this.drawEdges()}
-                    </Layer>
-                    <Layer>
-                        {this.drawStars()}
-                    </Layer>
-                </Stage>
-            )
-        } else {
-            let widthPadding = viewWidth < 900 ? 20 : 100;
-            let heightPadding = viewHeight < 750 ? 20 : 70;
-            let skyX = (viewWidth - skyWidth)/2;
-
-            return (
-                <Stage width={viewWidth} height={viewHeight}>
-                    <Layer>
-                        <Rect x={0} y={0} width={viewWidth} height={viewHeight} fill="#0E103D" />
-                    </Layer>
-                    <Layer>
-                        <Text ref={"text"} className="title" x={viewWidth/2 - 400}  y={0} fontSize={70}  fontFamily="Monofett" fill="#A9A9A9" text="Constellation Maker" />
-                        <Rect x={skyX} y={viewHeight - heightPadding - skyHeight} width={800} height={677} className="sky" fillPatternRepeat="no-repeat" fillPatternImage={this.state.fillPatternImage}
-                              onClick={(event) => this.update(event)}/>
-                    </Layer>
-                    <Layer ref={"edgesLayer"}>
-                        {this.drawEdges()}
-                    </Layer>
-                    <Layer>
-                        {this.drawStars()}
-                    </Layer>
-                </Stage>
-            )
-        }
-    }
-    
-
-render()
-{
-    return (
-        <div>
-            {this.getView()}
-        </div>
-    )
-}
+	render() {
+		return (
+			<div>
+				<Stage id="skyStage" height={this.state.stageHeight} width={this.state.stageWidth} x={this.state.scale} y={this.state.scale} style={{
+					margin: '0 auto',
+					borderRadius: '30px', width: '85%', overflow: 'hidden'}} >
+					<Layer>
+						<Image className="sky" width={this.state.stageWidth} height={this.state.stageHeight} x={this.state.scale} y={this.state.scale}
+							  fillPatternImage={this.state.fillPatternImage}
+							  onClick={(event) => this.update(event)}/>
+					</Layer>
+					<Layer ref={"edgesLayer"}>
+						{this.drawEdges()}
+					</Layer>
+					<Layer>
+						{this.drawStars()}
+					</Layer>
+				</Stage>
+			</div>
+		)
+	}
 }
 
 export default Sky;
